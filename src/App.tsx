8ede3162c16dc654e2b2f7d32e66a5c8c22c7a21
Daniel Escoto <escotodanny62@gmail.com>
2022-11-10 17:18:58 -0800

    improve api and add config

--- src/App.tsx
+++ src/App.tsx
@@ -1,90 +1,52 @@
 import { useEffect, useState } from "react";
+import { getToken, getTopTracksAndArtists } from "./components/Api";
+import {
+  AUTH_ENDPOINT,
+  CLIENT_ID,
+  REDIRECT_URI,
+  RESPONSE_TYPE,
+} from "./components/Config";
+import { TopTracksAndArtists } from "./components/Interfaces";
+
 import NavBar from "./components/NavBar";
 
-const CLIENT_ID = "a9f3d6f7725b4767ac02c448af1cf31a";
-const REDIRECT_URI = "http://127.0.0.1:5173/";
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks";
-const RESPONSE_TYPE = "token";
+interface State {
+  token: string;
+  topTracksAndArtists: TopTracksAndArtists | null;
+}
 
 function App() {
-  const [token, setToken] = useState("");
-  const [shortTermTracks, setShortTermTracks] = useState([]);
-  const [mediumTermTracks, setMediumTermTracks] = useState([]);
-  const [longTermTracks, setLongTermTracks] = useState([]);
-
-  const getTopTracks = async () => {
-    if (token.length === 0) return;
-
-    const shortTermResponse = await fetch(
-      `${TOP_TRACKS_ENDPOINT}?time_range=short_term&limit=50`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const shortTermData = await shortTermResponse.json();
-    setShortTermTracks(shortTermData.items);
-
-    const mediumTermResponse = await fetch(
-      `${TOP_TRACKS_ENDPOINT}?time_range=medium_term&limit=50`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const mediumTermData = await mediumTermResponse.json();
-    setMediumTermTracks(mediumTermData.items);
-
-    const longTermResponse = await fetch(
-      `${TOP_TRACKS_ENDPOINT}?time_range=long_term&limit=50`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
-
-    const longTermData = await longTermResponse.json();
-    setLongTermTracks(longTermData.items);
-  };
+  const [state, setState] = useState<State>({
+    token: "",
+    topTracksAndArtists: null,
+  });
 
   useEffect(() => {
-    const hash = window.location.hash;
-    let token = window.localStorage.getItem("token") || "";
+    const token = getToken();
 
-    if (!token && hash) {
-      token =
-        hash
-          .substring(1)
-          .split("&")
-          .find((item) => item.startsWith("access_token"))
-          ?.split("=")[1] ?? "";
-
-      window.location.hash = "";
-      window.localStorage.setItem("token", token);
+    if (token) {
+      setState({ ...state, token });
     }
-    setToken(token);
   }, []);
 
   useEffect(() => {
-    getTopTracks();
-  }, [token]);
+    if (state.token) {
+      getTopTracksAndArtists(state.token).then((topTracksAndArtists) => {
+        setState({ ...state, topTracksAndArtists });
+      });
+    }
+  }, [state.token]);
 
   const logOut = () => {
     window.localStorage.removeItem("token");
-    setToken("");
+    setState({ ...state, token: "" });
     window.location.reload();
   };
 
   return (
     // layout that includes the navbar, using tailwind
     <div className="flex flex-col h-screen">
-      <NavBar handleLogOut={logOut} token={token} />
+      <NavBar handleLogOut={logOut} token={state.token} />
       <div className="flex-grow">
         {/* welcome to Statsify */}
         <div className="flex flex-col items-center justify-center h-full">
@@ -93,7 +55,7 @@ function App() {
             A Spotify app that shows your top tracks
           </p>
           <div className="flex flex-col items-center justify-center mt-4 space-y-4">
-            {!token && (
+            {!state.token && (
               <a
                 href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read`}
                 className="px-4 py-2 text-white bg-green-500 rounded-md"
---
+++ src/components/Api.ts
@@ -0,0 +1,100 @@
+import { TopArtists, TopTracks, TopTracksAndArtists } from "./Interfaces";
+import {
+  CLIENT_ID,
+  REDIRECT_URI,
+  AUTH_ENDPOINT,
+  TOP_TRACKS_ENDPOINT,
+  TOP_ARTISTS_ENDPOINT,
+  RESPONSE_TYPE,
+} from "./Config";
+
+export async function getTopTracks(token: string): Promise<TopTracks> {
+  const shortTermTracks = await fetch(
+    `${TOP_TRACKS_ENDPOINT}?time_range=short_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  const mediumTermTracks = await fetch(
+    `${TOP_TRACKS_ENDPOINT}?time_range=medium_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  const longTermTracks = await fetch(
+    `${TOP_TRACKS_ENDPOINT}?time_range=long_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  return {
+    shortTermItems: shortTermTracks.items,
+    mediumTermItems: mediumTermTracks.items,
+    longTermItems: longTermTracks.items,
+  };
+}
+
+export async function getTopArtists(token: string): Promise<TopArtists> {
+  const shortTermArtists = await fetch(
+    `${TOP_ARTISTS_ENDPOINT}?time_range=short_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  const mediumTermArtists = await fetch(
+    `${TOP_ARTISTS_ENDPOINT}?time_range=medium_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  const longTermArtists = await fetch(
+    `${TOP_ARTISTS_ENDPOINT}?time_range=long_term&limit=50`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  ).then((response) => response.json());
+  return {
+    shortTermItems: shortTermArtists.items,
+    mediumTermItems: mediumTermArtists.items,
+    longTermItems: longTermArtists.items,
+  };
+}
+
+export async function getTopTracksAndArtists(
+  token: string
+): Promise<TopTracksAndArtists> {
+  const topTracks = await getTopTracks(token);
+  const topArtists = await getTopArtists(token);
+  return { topTracks, topArtists };
+}
+
+export function getToken(): string {
+  const hash = window.location.hash;
+  let token = window.localStorage.getItem("token") || "";
+
+  if (!token && hash) {
+    token =
+      hash
+        .substring(1)
+        .split("&")
+        .find((item) => item.startsWith("access_token"))
+        ?.split("=")[1] ?? "";
+
+    window.location.hash = "";
+    window.localStorage.setItem("token", token);
+  }
+
+  return token;
+}
