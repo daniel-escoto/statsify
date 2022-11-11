fd73bbf26e18cef05d2a2ba77ee070a146923e57
Daniel Escoto <escotodanny62@gmail.com>
2022-11-10 16:01:10 -0800

    get top songs

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,54 @@ import { useEffect, useState } from "react";
 const CLIENT_ID = "a9f3d6f7725b4767ac02c448af1cf31a";
 const REDIRECT_URI = "http://127.0.0.1:5173/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+const TOP_TRACKS_ENDPOINT = "https://api.spotify.com/v1/me/top/tracks";
 const RESPONSE_TYPE = "token";
 
 function App() {
   const [token, setToken] = useState("");
+  const [shortTermTracks, setShortTermTracks] = useState([]);
+  const [mediumTermTracks, setMediumTermTracks] = useState([]);
+  const [longTermTracks, setLongTermTracks] = useState([]);
+
+  const getTopTracks = async () => {
+    if (token.length === 0) return;
+
+    const shortTermResponse = await fetch(
+      `${TOP_TRACKS_ENDPOINT}?time_range=short_term&limit=50`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const shortTermData = await shortTermResponse.json();
+    setShortTermTracks(shortTermData.items);
+
+    const mediumTermResponse = await fetch(
+      `${TOP_TRACKS_ENDPOINT}?time_range=medium_term&limit=50`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const mediumTermData = await mediumTermResponse.json();
+    setMediumTermTracks(mediumTermData.items);
+
+    const longTermResponse = await fetch(
+      `${TOP_TRACKS_ENDPOINT}?time_range=long_term&limit=50`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+
+    const longTermData = await longTermResponse.json();
+    setLongTermTracks(longTermData.items);
+  };
 
   useEffect(() => {
     const hash = window.location.hash;
@@ -25,6 +69,10 @@ function App() {
     }
     setToken(token);
   }, []);
+
+  useEffect(() => {
+    getTopTracks();
+  }, [token]);
 
   const logOut = () => {
     window.localStorage.removeItem("token");
@@ -49,7 +97,7 @@ function App() {
           <h1 className="text-2xl font-bold">Login to Spotify</h1>
           <a
             className="px-4 py-2 mt-4 text-white bg-green-500 rounded hover:bg-green-600"
-            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-read-private%20user-read-email`}
+            href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=user-top-read%20user-read-email`}
           >
             Login
           </a>
