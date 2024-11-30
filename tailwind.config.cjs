caef0e56c243d0ba3d021d06a39d1d59dfd8bc8b
Daniel Escoto <escotodanny62@gmail.com>
2024-11-30 16:24:52 -0500

    inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif", "system-ui"],
+      },
+    },
   },
   plugins: [],
 };
