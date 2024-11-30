981abf819454ce4d1fde6f81aad5c9bf49a5cc3c
Daniel Escoto <escotodanny62@gmail.com>
2024-11-30 14:31:22 -0500

    config

---
+++ src/utilities/Config.ts
@@ -0,0 +1,6 @@
+export const CLIENT_ID = import.meta.env.VITE_CLIENT_ID;
+export const REDIRECT_URI = import.meta.env.VITE_REDIRECT_URI;
+export const AUTH_ENDPOINT = import.meta.env.VITE_AUTH_ENDPOINT;
+export const TOP_TRACKS_ENDPOINT = import.meta.env.VITE_TOP_TRACKS_ENDPOINT;
+export const TOP_ARTISTS_ENDPOINT = import.meta.env.VITE_TOP_ARTISTS_ENDPOINT;
+export const RESPONSE_TYPE = import.meta.env.VITE_RESPONSE_TYPE;
