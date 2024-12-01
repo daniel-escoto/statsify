4d9e8e53391980a4136cb756b255ad05dc44c705
Daniel Escoto <escotodanny62@gmail.com>
2024-12-01 12:48:05 -0500

    hide duplicate login button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -29,12 +29,7 @@ export default function NavBar({ token, handleLogOut }: NavBarProps) {
                 Log Out
               </button>
             ) : (
-              <a
-                href={loginUrl}
-                className="bg-accent text-white px-4 py-2 rounded-md text-sm font-medium hover:bg-accent-dark transition focus:outline-none focus:ring-2 focus:ring-accent focus:ring-offset-2"
-              >
-                Log In
-              </a>
+              <></>
             )}
           </div>
         </div>
